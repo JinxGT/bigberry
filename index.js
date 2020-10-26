5ee50878d67d090a9b02713139f22b96f20c86de
Jinx-_-GT <70546159+JinxGT@users.noreply.github.com>
2020-10-25 19:12:30 -0600

    Update index.js

--- index.js
+++ index.js
@@ -84,6 +84,8 @@ bot.on("message", async message => {
 if (command === "say") {
 
         if (!args[0]) return message.channel.send("What should i say?, try `*say <message>`")
+	if (message.content.includes("^")) {
+            return message.reply("ERROR: the "^" key is invalid!")
         
         }
         const sayMessage = args.slice(0).join(" ");
